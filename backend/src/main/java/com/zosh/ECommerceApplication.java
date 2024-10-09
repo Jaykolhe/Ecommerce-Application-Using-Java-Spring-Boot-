1ab87bd6bf6ba3c2f951ee09019eff9aaffaf37e
Jaykolhe <123361018+Jaykolhe@users.noreply.github.com>
2024-10-09 17:45:17 +0530

    Update ECommerceApplication.java

--- backend/src/main/java/com/zosh/ECommerceApplication.java
+++ backend/src/main/java/com/zosh/ECommerceApplication.java
@@ -15,11 +15,11 @@ import io.swagger.v3.oas.annotations.info.License;
 		description="Welcome to the documentation for the eCommerce Project API! This API provides a set of endpoints and functionalities to interact with our eCommerce platform programmatically. With this API, you can build applications, integrations, and automate various tasks related to our online store.",
 		version="v1",
 		contact=@Contact(
-				name="Code With Zosh",
-				email="codewithzosh"
+				name="Code With Jay",
+				email="kolhejay68@gmail.com"
 				),
 		license=@License(
-				name="Code With Zosh"
+				name="Code With Jay"
 				)
 		))
 public class ECommerceApplication {
